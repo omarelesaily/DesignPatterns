667367674c4cb420e43401b0a1c1f572421a92e3
Omar Hassan <Omarelesaily1999@gmailc.om>
2024-08-24 16:50:10 +0300

    DesignPatterns : [Add] Prototype design pattern creational

---
+++ CreationalPatterns_Prototype/MainUser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System;
+using CreationalPatterns_Prototype.SubClasses;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sandwich sandwich1 = new Sandwich { ID=Guid.NewGuid(),Name = "Chicken Wrap", MainIngrediant = "Chicken", IsVegetarian = false,Components=new SandwichComponents(){ Bread = "Tortilla Wrap", Fillings = "Grilled Chicken Breast", Spread = "Caesar Dressing" } };
+        Sandwich sandwich2 = (Sandwich)sandwich1.Clone();
+
+        // Modifying the copied object's reference type field
+        sandwich2.MainIngrediant= "Meat";
+        sandwich2.ID = new Guid();
+        sandwich2.Components.Fillings = "Meat";
+
+        Console.WriteLine(sandwich1.MainIngrediant); // Output: Chicken, referance but immutable (meaning: can not be changed so a new oject is created with cloning)
+        Console.WriteLine(sandwich2.MainIngrediant); // Output: Meat
+
+        Console.WriteLine();
+
+        Console.WriteLine(sandwich1.ID);// Different 
+        Console.WriteLine(sandwich2.ID);// Different 00000000-0000-0000-0000-000000000000
+
+        Console.WriteLine();
+
+        Console.WriteLine(sandwich1.Components.Fillings);//OutPut : Meat
+        Console.WriteLine(sandwich2.Components.Fillings);//OutPut : Meat
+    }
+
+}
---
+++ CreationalPatterns_Prototype/SubClasses/Sandwich.cs
@@ -0,0 +1,34 @@
+﻿using CreationalPatterns_Prototype.ICloneable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPatterns_Prototype.SubClasses
+{
+    public class Sandwich : ICloneableSandwich
+    {
+        public Guid ID { get; set; } = new Guid();
+        public string Name { get; set; }
+        public string MainIngrediant { get; set; }
+        public bool IsVegetarian { get; set; }
+        public SandwichComponents Components { get; set; }
+
+        //shallow clone : refernce changes will change in all clones and original objects,
+        //be aware that some types are immutable that even if they are a reference type and
+        //you changed them they will not change any of the clones or the original object
+        //Example : string , the original will not be affected and changed as the clone changes
+        //Example : class the contian string property , original will be changed and affected as the clone changes
+        public ICloneableSandwich Clone()
+        {
+            return (ICloneableSandwich)this.MemberwiseClone();
+        }
+    }
+    public class SandwichComponents
+    {
+        public string Bread { get; set; }
+        public string Spread { get; set; }
+        public string Fillings { get; set; }
+    }
+}
